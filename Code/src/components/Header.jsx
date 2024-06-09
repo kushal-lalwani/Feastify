41e1e944e4b95786787a2cc586bcd0b202026d01
Kushal Lalwani <kushallalwani.kl@gmail.com>
2024-06-09 15:56:33 +0530

    Added Tailwind Styles

--- Code/src/components/Header.jsx
+++ Code/src/components/Header.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
     return (
-      <div className="header">
+      <div className="flex justify-between shadow-lg p-4 m-2">
         <div className="logo-container">
           <img
             src={Logo}
             alt="App Logo"
-            className="logo"
+            className="logo w-56"
           />
         </div>
         <div className="nav-items">
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/grocery'>Grocery</Link></li>
-            <li><Link to='/about'>About Us</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
-            <li>Cart</li>
+          <ul className="flex items-center">
+            <li className="px-4"><Link to='/'>Home</Link></li>
+            <li className="px-4"><Link to='/grocery'>Grocery</Link></li>
+            <li className="px-4"><Link to='/about'>About Us</Link></li>
+            <li className="px-4"><Link to='/contact'>Contact</Link></li>
+            <li className="px-4">Cart</li>
           </ul>
         </div>
       </div>
--- Code/src/components/RestaurantCard.jsx
+++ Code/src/components/RestaurantCard.jsx
@@ -2,13 +2,13 @@ import { IMAGE_URL } from "../utils/constants"
 import { Link } from "react-router-dom"
 
 export const RestaurantCard = ({cloudinaryImageId,name,cuisines,areaName, id}) => {
-
+    
     return(
-      <div className="card">
-      <img src={IMAGE_URL+cloudinaryImageId}/>
-      <Link to={"/restaurant/" + id}><h2>{name}</h2></Link>
-      <h3>{cuisines.join(', ')}</h3>
-      <h4>{areaName}</h4>
+      <div className="w-52 bg-gray-100 hover:bg-[#f0f0f0] hover:shadow-lg m-4 p-4 rounded-xl">
+      <img src={IMAGE_URL+cloudinaryImageId} className="h-48 w-52 rounded-md"/>
+      <Link to={"/restaurant/" + id}><h2 className="font-bold text-lg py-1">{name}</h2></Link>
+      <h3 className="text-gray-700 py-1">{cuisines.join(', ')}</h3>
+      <h4 className="text-gray-600">{areaName}</h4>
     </div>
 )
 }
